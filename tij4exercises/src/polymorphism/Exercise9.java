09dee606b08145f45fb86dd1544aed7209c2b11a
AlexLixin <lixin1996919@126.com>
2018-12-05 01:34:48 +0800

    456

--- tij4exercises/src/polymorphism/Exercise9.java
+++ tij4exercises/src/polymorphism/Exercise9.java
@@ -96,7 +96,7 @@ public class Exercise9 {
 			r[i] = next();
 		}
 		f(r);
-		System.out.println("   fdsafdsfd45456sfsdafdsafasfdsafdsfa");
+		System.out.println("   fdsafdsfd45456sfsdfdsfdafdsafasfdsafdsfa");
 	}
 
 }
