b4f4d8e98ff8e4f9e39a478a9cd08af84ce38709
AlexLixin <lixin1996919@126.com>
2018-12-07 21:12:01 +0800

    Exercise14 finished!

--- tij4exercises/src/polymorphism/exercise/Exercise9.java
+++ tij4exercises/src/polymorphism/exercise/Exercise9.java
@@ -5,129 +5,182 @@ import java.util.Random;
 import static net.mindview.util.Print.*;
 
 class Description {
-	private String s;
+    private Shared shared;
+    private static long counter = 0;
+    private final long id = counter++;
+    private String s;
 
-	Description(String s) {
-		this.s = s;
-		print("Creating Description " + s);
-	}
+    Description(String s, Shared shared) {
+        this.s = s;
+        this.shared = shared;
+        this.shared.addRef();
+        print("Creating " + this); //$NON-NLS-1$
+    }
 
-	protected void dispose() {
-		print("disposing Description " + s);
-	}
+    protected void dispose() {
+        print("disposing " + this); //$NON-NLS-1$
+        shared.dispose();
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + " " + s + " " + id; //$NON-NLS-1$
+    }
+
 }
 
 class Rodent {
-	private Description d = new Description("rodent description");
-	private Shared s;
+    private Shared shared;
+    private static long counter = 0;
+    private final long id = counter++;
+    private Description d; //$NON-NLS-1$
 
-	public Rodent() {
-		print("Rodent()");
-	}
-	public Rodent(Shared s) {
-		print("Rodent()");
-	}
+    public Rodent() {
+        print("Rodent()"); //$NON-NLS-1$
+    }
 
-	void run() {
-		System.out.println("Rodent.run()");
-	}
+    public Rodent(Shared s) {
+        this.shared = s;
+        d = new Description("rodent description", this.shared);
+        print("Creating " + this); //$NON-NLS-1$
+        this.shared.addRef();
+    }
 
-	void eat() {
-		System.out.println("Rodent.eat()");
-	}
+    void run() {
+        System.out.println("Rodent.run()"); //$NON-NLS-1$
+    }
+
+    void eat() {
+        System.out.println("Rodent.eat()"); //$NON-NLS-1$
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + id;
+    }
 }
 
 class Mouse extends Rodent {
-	private Description d = new Description("Mouse description");
-	private Shared s;
+    private Shared shared;
+    private static long counter = 0;
+    private final long id = counter++;
+    private Description d; //$NON-NLS-1$
 
-	public Mouse(Shared s2) {
-		print("Mouse()");
-	}
+    public Mouse(Shared s2) {
+        print("Creating " + this); //$NON-NLS-1$
+        this.shared = s2;
+        d = new Description("Mouse description", this.shared);
+        this.shared.addRef();
+    }
 
-	public Mouse() {
-		print("Mouse()");
-	}
+    public Mouse() {
+        print("Mouse()"); //$NON-NLS-1$
+    }
 
-	void run() {
-		System.out.println("Mouse.run()");
-	}
+    void run() {
+        System.out.println("Mouse.run()"); //$NON-NLS-1$
+    }
 
-	void eat() {
-		System.out.println("Mouse.eat()");
-	}
+    void eat() {
+        System.out.println("Mouse.eat()"); //$NON-NLS-1$
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + id;
+    }
 }
 
 class Gerbil extends Rodent {
-	private Description d = new Description("Gerbil description");
-	private Shared s;
+    private Shared shared;
+    private static long counter = 0;
+    private final long id = counter++;
+    private Description d; //$NON-NLS-1$
 
-	public Gerbil(Shared s2) {
-		print("Gerbil()");
-	}
+    public Gerbil(Shared s2) {
+        print("Creating " + this); //$NON-NLS-1$
+        this.shared = s2;
+        d = new Description("Gerbil description", this.shared);
+        this.shared.addRef();
+    }
 
-	public Gerbil() {
-		print("Gerbil()");
-	}
+    public Gerbil() {
+        print("Gerbil()"); //$NON-NLS-1$
+    }
 
-	void run() {
-		System.out.println("Gerbil.run()");
-	}
+    void run() {
+        System.out.println("Gerbil.run()"); //$NON-NLS-1$
+    }
 
-	void eat() {
-		System.out.println("Gerbil.eat()");
-	}
+    void eat() {
+        System.out.println("Gerbil.eat()"); //$NON-NLS-1$
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + id;
+    }
 }
 
 class Hamster extends Rodent {
-	private Description d = new Description("Hamster description");
-	private Shared s;
+    private Shared shared;
+    private static long counter = 0;
+    private final long id = counter++;
+    private Description d; //$NON-NLS-1$
 
-	public Hamster() {
-		print("Hamster()");
-	}
+    public Hamster() {
+        print("Hamster()"); //$NON-NLS-1$
+    }
 
-	public Hamster(Shared s2) {
-		print("Hamster()");
-	}
+    public Hamster(Shared s2) {
+        print("Creating " + this); //$NON-NLS-1$
+        this.shared = s2;
+        d = new Description("Hamster description", this.shared);
+        this.shared.addRef();
+    }
 
-	void run() {
-		System.out.println("Hamster.run()");
-	}
+    void run() {
+        System.out.println("Hamster.run()"); //$NON-NLS-1$
+    }
 
-	void eat() {
-		System.out.println("Hamster.eat()");
-	}
+    void eat() {
+        System.out.println("Hamster.eat()"); //$NON-NLS-1$
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + id;
+    }
 }
 
 public class Exercise9 {
-	private static Random rand = new Random();
+    private static Random rand = new Random();
 
-	public static Rodent next() {
-		switch (rand.nextInt(3)) {
-		default:
-		case 0:
-			return new Mouse();
-		case 1:
-			return new Gerbil();
-		case 2:
-			return new Hamster();
-		}
-	}
+    public static Rodent next() {
+        switch (rand.nextInt(3)) {
+            default:
+            case 0:
+                return new Mouse();
+            case 1:
+                return new Gerbil();
+            case 2:
+                return new Hamster();
+        }
+    }
 
-	static public void f(Rodent[] r) {
-		for (Rodent rodent : r) {
-			rodent.eat();
-			rodent.run();
-		}
-	}
+    static public void f(Rodent[] r) {
+        for (Rodent rodent : r) {
+            rodent.eat();
+            rodent.run();
+        }
+    }
 
-	public static void main(String[] args) {
-		Rodent[] r = new Rodent[3];
-		for (int i = 0; i < r.length; i++) {
-			r[i] = next();
-		}
-		f(r);
-	}
+    public static void main(String[] args) {
+        Rodent[] r = new Rodent[3];
+        for (int i = 0; i < r.length; i++) {
+            r[i] = next();
+        }
+        f(r);
+    }
 
 }
