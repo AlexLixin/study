306155490135dd604e8f07fda098514fdc8cf8ba
AlexLixin <lixin1996919@126.com>
2018-12-09 23:54:37 +0800

    20181209

--- tij4exercises/src/polymorphism/PolyConstructors.java
+++ tij4exercises/src/polymorphism/PolyConstructors.java
@@ -1,35 +1,56 @@
 package polymorphism;
+
 // Constructors and polymorphism
 // don't produce what you might expect.
 import static net.mindview.util.Print.*;
 
 class Glyph {
-  void draw() { print("Glyph.draw()"); }
-  Glyph() {
-    print("Glyph() before draw()");
-    draw();
-    print("Glyph() after draw()");
-  }
-}	
+	void draw() {
+		print("Glyph.draw()");
+	}
+
+	Glyph() {
+		print("Glyph() before draw()");
+		draw();
+		print("Glyph() after draw()");
+	}
+}
 
 class RoundGlyph extends Glyph {
-  private int radius = 1;
-  RoundGlyph(int r) {
-    radius = r;
-    print("RoundGlyph.RoundGlyph(), radius = " + radius);
-  }
-  void draw() {
-    print("RoundGlyph.draw(), radius = " + radius);
-  }
-}	
+	private int radius = 1;
+
+	RoundGlyph(int r) {
+		radius = r;
+		print("RoundGlyph.RoundGlyph(), radius = " + radius);
+	}
+
+	void draw() {
+		print("RoundGlyph.draw(), radius = " + radius);
+	}
+}
+
+class RectangularGlyph extends Glyph {
+	private int radius = 9;
+
+	public RectangularGlyph(int r) {
+		super();
+		radius = r;
+		print("RectangularGlyph.draw(),radius = " + radius);
+	}
+
+	void draw() {
+		print("RectangularGlyph.draw(), radius = " + radius);
+
+	}
+
+}
 
 public class PolyConstructors {
-  public static void main(String[] args) {
-    new RoundGlyph(5);
-  }
-} /* Output:
-Glyph() before draw()
-RoundGlyph.draw(), radius = 0
-Glyph() after draw()
-RoundGlyph.RoundGlyph(), radius = 5
-*///:~
+	public static void main(String[] args) {
+		new RoundGlyph(5);
+		new RectangularGlyph(1131);
+	}
+} /*
+	 * Output: Glyph() before draw() RoundGlyph.draw(), radius = 0 Glyph() after
+	 * draw() RoundGlyph.RoundGlyph(), radius = 5
+	 */// :~
