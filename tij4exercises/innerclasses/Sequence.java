22640feaab2dd4d1ded1c99f617f3589d349bea1
AlexLixin <lixin1996919@126.com>
2018-12-14 10:00:33 +0800

    Interface finished!

---
+++ tij4exercises/innerclasses/Sequence.java
@@ -0,0 +1,47 @@
+import exercises.Exercise2;
+
+//: innerclasses/Sequence.java
+// Holds a sequence of Objects.
+
+interface Selector {
+  boolean end();
+  Object current();
+  void next();
+}
+
+public class Sequence {
+  private Object[] items;
+  private int next = 0;
+  public Sequence(int size) { items = new Object[size]; }
+  public void add(Object x) {
+    if(next < items.length)
+      items[next++] = x;
+  }
+  private class SequenceSelector implements Selector {
+    private int i = 0;
+    public boolean end() { return i == items.length; }
+    public Object current() { return items[i]; }
+    public void next() { if(i < items.length) i++; }
+    public Sequence getOuter() {
+        return Sequence.this;
+    }
+  }
+  public Selector selector() {
+    return new SequenceSelector();
+  }
+  public static void main(String[] args) {
+    Sequence sequence = new Sequence(13);
+    for(int i = 0; i < 10; i++)
+      sequence.add(Integer.toString(i));
+    sequence.add(new Exercise2().getExer2());
+    sequence.add(new Exercise2().getExer2());
+    sequence.add(new Exercise2().getExer2());
+    Selector selector = sequence.selector();
+    while(!selector.end()) {
+      System.out.print(selector.current() + " ");
+      selector.next();
+    }
+  }
+} /* Output:
+0 1 2 3 4 5 6 7 8 9
+*///:~
