7cb4e1539712ce084dbcdfb75be29326022abc84
AlexLixin <lixin1996919@126.com>
2018-12-14 20:57:32 +0800

    Inner Classes  Exercise11

--- tij4exercises/innerclasses/exercises/Exercise11.java
+++ tij4exercises/innerclasses/exercises/Exercise11.java
@@ -19,8 +19,33 @@
 package exercises;
 
 interface Exer11_1{
+    void f();
+}
 
+class Exer11_2{
+    private class Exer11_3 implements Exer11_1{
+
+        @Override
+        public void f() {
+            System.out.println("f()");
+        }
+
+        public void g() {
+            System.out.println("g()");
+        }
+
+    }
+    Exer11_1 getExer11_3(){
+        return new Exer11_3();
+    }
 }
 public class Exercise11 {
+    public static void main(String[] args) {
+        Exer11_2 exer11_2 = new Exer11_2();
+        Exer11_1 exer11_1 = exer11_2.getExer11_3();
+        exer11_1.f();
+        //(Exer11_3)exer11_1.g();
+
+    }
 
 }
