e486d6daba184b1d3e509e5c995b56c8fb4da750
AlexLixin <lixin1996919@126.com>
2018-12-15 12:22:56 +0800

    20181215 Inner Classes : exercise15

---
+++ tij4exercises/innerclasses/exercises/Exercise12.java
@@ -0,0 +1,45 @@
+package exercises;
+
+import exercises.Exer71.Exer72;
+
+class Exer12_1 {
+
+	private String s;
+
+	public Exer12_1(String s) {
+		this.s = s;
+	}
+
+	private void f() {
+		System.out.println("f()"); //$NON-NLS-1$
+	}
+	
+	abstract class Exer72{
+		abstract void run();
+	}
+
+	void g() {
+		System.out.println("g()"); //$NON-NLS-1$
+		new Exer72() {
+			void run() {
+				System.out.println("Exer72.run()"); //$NON-NLS-1$
+				s = "fdsafdsa"; //$NON-NLS-1$
+				f();
+			}
+		}.run();
+	}
+
+	void show() {
+		System.out.println(s);
+	}
+
+}
+
+public class Exercise12 {
+	public static void main(String[] args) {
+		Exer71 e = new Exer71("xixix"); //$NON-NLS-1$
+		e.show();
+		e.g();
+		e.show();
+	}
+}
---
+++ tij4exercises/innerclasses/exercises/Exercise14.java
@@ -0,0 +1,64 @@
+package exercises;
+
+interface Monster {
+	void menace();
+}
+
+interface DangerousMonster extends Monster {
+	void destroy();
+}
+
+interface Lethal {
+	void kill();
+}
+
+interface Vampire extends DangerousMonster, Lethal {
+	void drinkBlood();
+}
+
+public class Exercise14 {
+	static void u(Monster b) {
+		b.menace();
+	}
+
+	static void v(DangerousMonster d) {
+		d.menace();
+		d.destroy();
+	}
+
+	static void w(Lethal l) {
+		l.kill();
+	}
+
+	public static void main(String[] args) {
+		DangerousMonster barney = new DangerousMonster() {
+
+			public void menace() {
+			}
+
+			public void destroy() {
+			}
+
+		};
+		u(barney);
+		v(barney);
+		Vampire vlad = new Vampire() {
+
+			public void menace() {
+			}
+
+			public void destroy() {
+			}
+
+			public void kill() {
+			}
+
+			public void drinkBlood() {
+			}
+
+		};
+		u(vlad);
+		v(vlad);
+		w(vlad);
+	}
+} /// :~
