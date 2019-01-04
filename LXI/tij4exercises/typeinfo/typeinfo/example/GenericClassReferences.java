a582b0d6d3bc6d76e24f643131ae3a008fba6d11
AlexLixin <lixin1996919@126.com>
2019-01-04 20:56:19 +0800

    GenericClassReferences

---
+++ LXI/tij4exercises/typeinfo/typeinfo/example/GenericClassReferences.java
@@ -0,0 +1,29 @@
+/************************************************************************
+ *                                                                      *
+ *  DDDD     SSSS    AAA        Daten- und Systemtechnik Aachen GmbH    *
+ *  D   D   SS      A   A       Pascalstrasse 28                        *
+ *  D   D    SSS    AAAAA       52076 Aachen-Oberforstbach, Germany     *
+ *  D   D      SS   A   A       Telefon: +49 (0)2408 / 9492-0           *
+ *  DDDD    SSSS    A   A       Telefax: +49 (0)2408 / 9492-92          *
+ *                                                                      *
+ *                                                                      *
+ *  (c) Copyright by DSA - all rights reserved                          *
+ *                                                                      *
+ ************************************************************************
+ *
+ * Initial Creation:
+ *    Author      LXI
+ *    Created on  Jan 4, 2019
+ *
+ ************************************************************************/
+package typeinfo.example;
+
+public class GenericClassReferences {
+    public static void main(String[] args) {
+        Class intClass = int.class;
+        Class<Integer> genericIntClass = int.class;
+        genericIntClass = Integer.class; // Same thing
+        intClass = double.class;
+        // genericIntClass = double.class; // Illegal
+    }
+} ///:~
