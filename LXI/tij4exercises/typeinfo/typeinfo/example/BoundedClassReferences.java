b75ccac3794ddf9acbcf9c06c3b3ba493612e7d8
AlexLixin <lixin1996919@126.com>
2019-01-07 19:55:53 +0800

    Type information: P.405

---
+++ LXI/tij4exercises/typeinfo/typeinfo/example/BoundedClassReferences.java
@@ -0,0 +1,28 @@
+/************************************************************************
+ *                                                                      *
+ *  DDDD     SSSS    AAA        Daten- und Systemtechnik Aachen GmbH    *
+ *  D   D   SS      A   A       Pascalstrasse 28                        *
+ *  D   D    SSS    AAAAA       52076 Aachen-Oberforstbach, Germany     *
+ *  D   D      SS   A   A       Telefon: +49 (0)2408 / 9492-0           *
+ *  DDDD    SSSS    A   A       Telefax: +49 (0)2408 / 9492-92          *
+ *                                                                      *
+ *                                                                      *
+ *  (c) Copyright by DSA - all rights reserved                          *
+ *                                                                      *
+ ************************************************************************
+ *
+ * Initial Creation:
+ *    Author      LXI
+ *    Created on  Jan 7, 2019
+ *
+ ************************************************************************/
+package typeinfo.example;
+
+public class BoundedClassReferences {
+    public static void main(String[] args) {
+        Class<? extends Number> bounded = int.class;
+        bounded = double.class;
+        bounded = Number.class;
+        // Or anything else derived from Number.
+    }
+} ///:~
