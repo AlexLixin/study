3e4385fe07042a43319f52d77b01f5a0978e8520
AlexLixin <lixin1996919@126.com>
2019-01-09 20:10:57 +0800

    Type information : dynamic proxy

--- LXI/tij4exercises/typeinfo/typeinfo/SimpleProxyDemo.java
+++ LXI/tij4exercises/typeinfo/typeinfo/SimpleProxyDemo.java
@@ -1,48 +1,53 @@
 package typeinfo;
+
 //: typeinfo/SimpleProxyDemo.java
 import static net.mindview.util.Print.*;
 
 interface Interface {
-  void doSomething();
-  void somethingElse(String arg);
+	void doSomething();
+
+	void somethingElse(String arg);
 }
 
 class RealObject implements Interface {
-  public void doSomething() { print("doSomething"); }
-  public void somethingElse(String arg) {
-    print("somethingElse " + arg);
-  }
-}	
+	public void doSomething() {
+		print("doSomething");
+	}
+
+	public void somethingElse(String arg) {
+		print("somethingElse " + arg);
+	}
+}
 
 class SimpleProxy implements Interface {
-  private Interface proxied;
-  public SimpleProxy(Interface proxied) {
-    this.proxied = proxied;
-  }
-  public void doSomething() {
-    print("SimpleProxy doSomething");
-    proxied.doSomething();
-  }
-  public void somethingElse(String arg) {
-    print("SimpleProxy somethingElse " + arg);
-    proxied.somethingElse(arg);
-  }
-}	
+	private Interface proxied;
+
+	public SimpleProxy(Interface proxied) {
+		this.proxied = proxied;
+	}
+
+	public void doSomething() {
+		print("SimpleProxy doSomething");
+		proxied.doSomething();
+	}
+
+	public void somethingElse(String arg) {
+		print("SimpleProxy somethingElse " + arg);
+		proxied.somethingElse(arg);
+	}
+}
 
 class SimpleProxyDemo {
-  public static void consumer(Interface iface) {
-    iface.doSomething();
-    iface.somethingElse("bonobo");
-  }
-  public static void main(String[] args) {
-    consumer(new RealObject());
-    consumer(new SimpleProxy(new RealObject()));
-  }
-} /* Output:
-doSomething
-somethingElse bonobo
-SimpleProxy doSomething
-doSomething
-SimpleProxy somethingElse bonobo
-somethingElse bonobo
-*///:~
+	public static void consumer(Interface iface) {
+		iface.doSomething();
+		iface.somethingElse("bonobo");
+	}
+
+	public static void main(String[] args) {
+		consumer(new RealObject());
+		consumer(new SimpleProxy(new RealObject()));
+	}
+} /*
+	 * Output: doSomething somethingElse bonobo SimpleProxy doSomething doSomething
+	 * SimpleProxy somethingElse bonobo somethingElse bonobo
+	 */// :~
--- LXI/tij4exercises/typeinfo/typeinfo/toys/ToyTest.java
+++ LXI/tij4exercises/typeinfo/typeinfo/toys/ToyTest.java
@@ -1,70 +1,94 @@
 //: typeinfo/toys/ToyTest.java
 // Testing class Class.
 package typeinfo.toys;
+
 import static net.mindview.util.Print.*;
 
-interface HasBatteries {}
-interface Waterproof {}
-interface Shoots {}
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
-class Toy {
-  // Comment out the following default constructor
-  // to see NoSuchMethodError from (*1*)
-  Toy() {}
-  Toy(int i) {}
+interface HasBatteries {
 }
 
-class FancyToy extends Toy
-implements HasBatteries, Waterproof, Shoots {
-  FancyToy() { super(1); }
+interface Waterproof {
+}
+
+interface Shoots {
+}
+
+class Toy {
+	// Comment out the following default constructor
+	// to see NoSuchMethodError from (*1*)
+	Toy() {
+	}
+
+	Toy(int i) {
+	}
+}
+
+class FancyToy extends Toy implements HasBatteries, Waterproof, Shoots {
+	FancyToy() {
+		super(1);
+	}
 }
 
 public class ToyTest {
-  static void printInfo(Class cc) {
-    print("Class name: " + cc.getName() +
-      " is interface? [" + cc.isInterface() + "]");
-    print("Simple name: " + cc.getSimpleName());
-    print("Canonical name : " + cc.getCanonicalName());
-  }
-  public static void main(String[] args) {
-    Class c = null;
-    try {
-      c = Class.forName("typeinfo.toys.FancyToy");
-    } catch(ClassNotFoundException e) {
-      print("Can't find FancyToy");
-      System.exit(1);
-    }
-    printInfo(c);	
-    for(Class face : c.getInterfaces())
-      printInfo(face);
-    Class up = c.getSuperclass();
-    Object obj = null;
-    try {
-      // Requires default constructor:
-      obj = up.newInstance();
-    } catch(InstantiationException e) {
-      print("Cannot instantiate");
-      System.exit(1);
-    } catch(IllegalAccessException e) {
-      print("Cannot access");
-      System.exit(1);
-    }
-    printInfo(obj.getClass());
-  }
-} /* Output:
-Class name: typeinfo.toys.FancyToy is interface? [false]
-Simple name: FancyToy
-Canonical name : typeinfo.toys.FancyToy
-Class name: typeinfo.toys.HasBatteries is interface? [true]
-Simple name: HasBatteries
-Canonical name : typeinfo.toys.HasBatteries
-Class name: typeinfo.toys.Waterproof is interface? [true]
-Simple name: Waterproof
-Canonical name : typeinfo.toys.Waterproof
-Class name: typeinfo.toys.Shoots is interface? [true]
-Simple name: Shoots
-Canonical name : typeinfo.toys.Shoots
-Class name: typeinfo.toys.Toy is interface? [false]
-Simple name: Toy
-Canonical name : typeinfo.toys.Toy
-*///:~
+	static void printInfo(Class<?> cc) {
+		print("Class name: " + cc.getName() + " is interface? [" + cc.isInterface() + "]");
+		print("Simple name: " + cc.getSimpleName());
+		print("Canonical name : " + cc.getCanonicalName());
+	}
+
+	public static void main(String[] args) {
+		Class<?> c = null;
+		try {
+			c = Class.forName("typeinfo.toys.FancyToy");
+		} catch (ClassNotFoundException e) {
+			print("Can't find FancyToy");
+			System.exit(1);
+		}
+		printInfo(c);
+		for (Class<?> face : c.getInterfaces())
+			printInfo(face);
+		Class<?> up = c.getSuperclass();
+		Object obj = null;
+		try {
+			// Requires default constructor:
+			obj = up.newInstance();
+			Constructor<?>[] constructors = up.getConstructors();
+			for (Constructor<?> constructor : constructors) {
+				int parameterCount = constructor.getParameterCount();
+				if (parameterCount != 0) {
+					try {
+						obj = constructor.newInstance(4321);
+					} catch (IllegalArgumentException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (InvocationTargetException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+
+			}
+			if(obj instanceof Toy)System.out.println((Toy)obj);
+		} catch (InstantiationException e) {
+			print("Cannot instantiate");
+			System.exit(1);
+		} catch (IllegalAccessException e) {
+			print("Cannot access");
+			System.exit(1);
+		}
+		printInfo(obj.getClass());
+	}
+} /*
+	 * Output: Class name: typeinfo.toys.FancyToy is interface? [false] Simple name:
+	 * FancyToy Canonical name : typeinfo.toys.FancyToy Class name:
+	 * typeinfo.toys.HasBatteries is interface? [true] Simple name: HasBatteries
+	 * Canonical name : typeinfo.toys.HasBatteries Class name:
+	 * typeinfo.toys.Waterproof is interface? [true] Simple name: Waterproof
+	 * Canonical name : typeinfo.toys.Waterproof Class name: typeinfo.toys.Shoots is
+	 * interface? [true] Simple name: Shoots Canonical name : typeinfo.toys.Shoots
+	 * Class name: typeinfo.toys.Toy is interface? [false] Simple name: Toy
+	 * Canonical name : typeinfo.toys.Toy
+	 */// :~
