89f748bfe68d630f967ce65fa724ce084a7c4577
AlexLixin <lixin1996919@126.com>
2019-01-30 19:13:26 +0800

    A Set utility

---
+++ LXI/tij4exercises/generics/generics/watercolors/Watercolors.java
@@ -0,0 +1,27 @@
+/************************************************************************
+ *                                                                      *
+ *  DDDD     SSSS    AAA        Daten- und Systemtechnik Aachen GmbH    *
+ *  D   D   SS      A   A       Pascalstrasse 28                        *
+ *  D   D    SSS    AAAAA       52076 Aachen-Oberforstbach, Germany     *
+ *  D   D      SS   A   A       Telefon: +49 (0)2408 / 9492-0           *
+ *  DDDD    SSSS    A   A       Telefax: +49 (0)2408 / 9492-92          *
+ *                                                                      *
+ *                                                                      *
+ *  (c) Copyright by DSA - all rights reserved                          *
+ *                                                                      *
+ ************************************************************************
+ *
+ * Initial Creation:
+ *    Author      LXI
+ *    Created on  Jan 30, 2019
+ *
+ ************************************************************************/
+package generics.watercolors;
+
+public enum Watercolors {
+    ZINC, LEMON_YELLOW, MEDIUM_YELLOW, DEEP_YELLOW,
+    ORANGE, BRILLIANT_RED, CRIMSON, MAGENTA, ROSE_MADDER,
+    VIOLET, CERULEAN_BLUE_HUE, PHTHALO_BLUE, ULTRAMARINE,
+    COBALT_BLUE_HUE, PERMANENT_GREEN, VIRIDIAN_HUE, SAP_GREEN,
+    YELLOW_OCHRE, BURNT_SIENNA, RAW_UMBER, BURNT_UMBER, PAYNES_GRAY, IVORY_BLACK
+} ///:~
