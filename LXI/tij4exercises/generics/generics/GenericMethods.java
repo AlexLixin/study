2cece9b4d3858fc650de4bcd762dc179f2825644
AlexLixin <lixin1996919@126.com>
2019-03-25 18:42:17 +0800

    Holdltem simply holds an object

--- LXI/tij4exercises/generics/generics/GenericMethods.java
+++ LXI/tij4exercises/generics/generics/GenericMethods.java
@@ -1,15 +1,4 @@
 /************************************************************************
- *                                                                      *
- *  DDDD     SSSS    AAA        Daten- und Systemtechnik Aachen GmbH    *
- *  D   D   SS      A   A       Pascalstrasse 28                        *
- *  D   D    SSS    AAAAA       52076 Aachen-Oberforstbach, Germany     *
- *  D   D      SS   A   A       Telefon: +49 (0)2408 / 9492-0           *
- *  DDDD    SSSS    A   A       Telefax: +49 (0)2408 / 9492-92          *
- *                                                                      *
- *                                                                      *
- *  (c) Copyright by DSA - all rights reserved                          *
- *                                                                      *
- ************************************************************************
  *
  * Initial Creation:
  *    Author      LXI
