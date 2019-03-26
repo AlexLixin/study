71e8a29b8dac8238a58561c0124f08745a19f6b3
AlexLixin <lixin1996919@126.com>
2019-03-26 14:40:42 +0800

    Wildcards

---
+++ LXI/tij4exercises/generics/generics/CompilerIntelligence.java
@@ -0,0 +1,20 @@
+/************************************************************************
+ *
+ * Initial Creation:
+ *    Author      LXI
+ *    Created on  Mar 26, 2019
+ *
+ ************************************************************************/
+package generics;
+
+//: generics/CompilerIntelligence.java
+import java.util.*;
+
+public class CompilerIntelligence {
+    public static void main(String[] args) {
+        List<? extends Fruit> flist = Arrays.asList(new Apple());
+        Apple a = (Apple)flist.get(0); // No warning
+        flist.contains(new Apple()); // Argument is ‘Object’
+        flist.indexOf(new Apple()); // Argument is ‘Object’
+    }
+} ///:~
