996b197c8a3ab4450d2cf4b033d59c6dfd9a9e2e
AlexLixin <lixin1996919@126.com>
2019-03-29 00:21:12 +0800

    RaggedArray

---
+++ LXI/tij4exercises/arrays/arrays/ThreeDWithNew.java
@@ -0,0 +1,21 @@
+/************************************************************************
+ *
+ * Initial Creation:
+ *    Author      LXI
+ *    Created on  2019Äê3ÔÂ28ÈÕ
+ *
+ ************************************************************************/
+package arrays;
+
+//: arrays/ThreeDWithNew.java
+import java.util.*;
+
+public class ThreeDWithNew {
+	public static void main(String[] args) {
+		//3-D array with fixed length:
+		int[][][] a = new int[2][2][4];
+		System.out.println(Arrays.deepToString(a));
+	}
+} /*
+	 * Output: [[[0, 0, 0, 0], [0, 0, 0, 0]], [[0, 0, 0, 0], [0, 0, 0, 0]]]
+	 */// :~
