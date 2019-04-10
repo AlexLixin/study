7b93a7e3b1325030fd5c3d69bcafe674bacdafe0
AlexLixin <lixin1996919@126.com>
2019-04-10 14:10:36 +0800

    Filtering a list

--- LXI/angular/APM-Start/src/app/app.module.ts
+++ LXI/angular/APM-Start/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.componenet';
 import { ngModuleJitUrl } from '@angular/compiler';
 import { NgModule } from '@angular/core';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
