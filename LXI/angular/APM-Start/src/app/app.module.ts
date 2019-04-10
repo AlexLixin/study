b2da40f152576495842a66ae7dd3da8ad7f66bf0
AlexLixin <lixin1996919@126.com>
2019-04-10 18:51:18 +0800

    navigation and routing basic

--- LXI/angular/APM-Start/src/app/app.module.ts
+++ LXI/angular/APM-Start/src/app/app.module.ts
@@ -6,18 +6,30 @@ import { NgModule } from '@angular/core';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent },
+      {path: 'products/:id', component: ProductDetailComponent },
+      {path: 'welcome', component: WelcomeComponent },
+      {path: '', redirectTo: 'welcome',pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome',pathMatch: 'full'}
+    ]) 
   ],
   bootstrap: [AppComponent]
 })
--- LXI/angular/APM-Start/src/app/products/product-list.componenet.ts
+++ LXI/angular/APM-Start/src/app/products/product-list.componenet.ts
@@ -3,7 +3,6 @@ import { IProduct } from './product';
 import { ProductService } from './product.service';
 
 @Component({
-    selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
@@ -26,30 +25,10 @@ export class ProductListComponent implements OnInit {
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        }
-    ];
+    products: IProduct[];
 
     constructor(private productService: ProductService) {
+
     }
 
     onRatingClicked(message: string): void {
@@ -68,9 +47,11 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
         this.productService.getProducts().subscribe(
-            products => this.products = products,
+            products => {
+                this.products = products,
+                this.filteredProducts = this.products;
+            },
             error => this.errorMessage = <any>error
         );
-        this.filteredProducts = this.products;
     }
 }
