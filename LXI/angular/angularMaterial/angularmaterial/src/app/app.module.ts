c6aa8224a4b1ddaaf947f9311733dbc1a94644c2
AlexLixin <lixin1996919@126.com>
2019-04-15 19:39:31 +0800

    Getting Started

--- LXI/angular/angularMaterial/angularmaterial/src/app/app.module.ts
+++ LXI/angular/angularMaterial/angularmaterial/src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { MaterialModule } from './shared/material.module';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
