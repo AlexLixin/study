07dca1336e637eab85510f4c6904827e3751a61c
AlexLixin <lixin1996919@126.com>
2019-04-22 23:42:16 +0800

    CollectionDataTest

--- LXI/tij4exercises/net/net/mindview/util/CollectionData.java
+++ LXI/tij4exercises/net/net/mindview/util/CollectionData.java
@@ -1,23 +1,25 @@
 //: net/mindview/util/CollectionData.java
 // A Collection filled with data using a generator object.
 package net.mindview.util;
+
 import java.util.*;
 
 public class CollectionData<T> extends ArrayList<T> {
-  /**
-     * COMMENT - Add concise description of the purpose of this field.
-     *           Description should go beyond the field's name.
-     *
-     * Comment for <code>serialVersionUID</code>
-     */
-    private static final long serialVersionUID = 1L;
-public CollectionData(Generator<T> gen, int quantity) {
-    for(int i = 0; i < quantity; i++)
-      add(gen.next());
-  }
-  // A generic convenience method:
-  public static <T> CollectionData<T>
-  list(Generator<T> gen, int quantity) {
-    return new CollectionData<T>(gen, quantity);
-  }
-} ///:~
+	/**
+	 * COMMENT - Add concise description of the purpose of this field. Description
+	 * should go beyond the field's name.
+	 *
+	 * Comment for <code>serialVersionUID</code>
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CollectionData(Generator<T> gen, int quantity) {
+		for (int i = 0; i < quantity; i++)
+			add(gen.next());
+	}
+
+	// A generic convenience method:
+	public static <T> CollectionData<T> list(Generator<T> gen, int quantity) {
+		return new CollectionData<T>(gen, quantity);
+	}
+} /// :~
