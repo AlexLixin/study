c265b467a08b481582a143861561f61f6f9a8f55
AlexLixin <lixin1996919@126.com>
2019-04-25 23:45:03 +0800

    Choosing an implementation

---
+++ LXI/tij4exercises/containers/containers/Groundhog2.java
@@ -0,0 +1,24 @@
+/************************************************************************
+ *
+ * Initial Creation:
+ *    Author      LXI
+ *    Created on  2019Äê4ÔÂ25ÈÕ
+ *
+ ************************************************************************/
+package containers;
+
+//A class that¡¯s used as a key in a HashMap
+//must override hashCode() and equals().
+public class Groundhog2 extends Groundhog {
+	public Groundhog2(int n) {
+		super(n);
+	}
+
+	public int hashCode() {
+		return number;
+	}
+
+	public boolean equals(Object o) {
+		return o instanceof Groundhog2 && (number == ((Groundhog2) o).number);
+	}
+} /// :~
