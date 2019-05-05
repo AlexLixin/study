da15a4fc7bc8dda1cf2e2ad6d26ca96b7e22655c
AlexLixin <lixin1996919@126.com>
2019-05-05 22:38:24 +0800

    Microbenchmarking dangers

--- LXI/tij4exercises/containers/containers/Tester.java
+++ LXI/tij4exercises/containers/containers/Tester.java
@@ -15,7 +15,7 @@ public class Tester<C> {
 	public static int fieldWidth = 8;
 	public static TestParam[] defaultParams = TestParam.array(10, 5000, 100, 5000, 1000, 5000, 10000, 500);
 
-//Override this to modify pre-test initialization:
+	//Override this to modify pre-test initialization:
 	protected C initialize(int size) {
 		return container;
 	}
