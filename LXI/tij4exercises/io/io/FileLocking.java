74cd118905e69e8ac405671f8055b4982fa6d6f1
AlexLixin <lixin1996919@126.com>
2019-05-22 23:28:49 +0800

    Java Archives

---
+++ LXI/tij4exercises/io/io/FileLocking.java
@@ -0,0 +1,29 @@
+/************************************************************************
+ *
+ * Initial Creation:
+ *    Author      LXI
+ *    Created on  May 22, 2019
+ *
+ ************************************************************************/
+package io;
+
+//: io/FileLocking.java
+import java.nio.channels.*;
+import java.util.concurrent.*;
+import java.io.*;
+
+public class FileLocking {
+	public static void main(String[] args) throws Exception {
+		FileOutputStream fos = new FileOutputStream("file.txt");
+		FileLock fl = fos.getChannel().tryLock();
+		if (fl != null) {
+			System.out.println("Locked File");
+			TimeUnit.MILLISECONDS.sleep(100);
+			fl.release();
+			System.out.println("Released Lock");
+		}
+		fos.close();
+	}
+} /*
+	 * Output: Locked File Released Lock
+	 */// :~
