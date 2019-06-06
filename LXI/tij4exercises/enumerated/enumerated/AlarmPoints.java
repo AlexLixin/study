e51af4fb3e9e26187f625508c4021aff49e639ff
AlexLixin <lixin1996919@126.com>
2019-06-06 22:36:47 +0800

    Using EnumMap

---
+++ LXI/tij4exercises/enumerated/enumerated/AlarmPoints.java
@@ -0,0 +1,13 @@
+/************************************************************************
+ *
+ * Initial Creation:
+ *    Author      LXI
+ *    Created on  Jun 6, 2019
+ *
+ ************************************************************************/
+package enumerated;
+
+//: enumerated/AlarmPoints.java
+public enum AlarmPoints {
+	STAIR1, STAIR2, LOBBY, OFFICE1, OFFICE2, OFFICE3, OFFICE4, BATHROOM, UTILITY, KITCHEN
+} /// :~
