574774793fae2fc1959084b4847210aed07e9267
AlexLixin <lixin1996919@126.com>
2019-07-02 22:34:48 +0800

    Sleeping

--- tij4exercises/containers/containers/exercises/Exercise15.java
+++ tij4exercises/containers/containers/exercises/Exercise15.java
@@ -14,7 +14,8 @@ import net.mindview.util.TextFile;
 
 public class Exercise15 {
 	public static void main(String[] args) {
-		String[] textFile = TextFile.read("C:\\Users\\LXI\\Desktop\\tmp\\abc.txt").replaceAll("[().,]", " ").split("\\s+");
+		String[] textFile = TextFile.read("C:\\Users\\LXI\\Desktop\\tmp\\abc.txt").replaceAll("[().,]", " ")
+				.split("\\s+");
 		SlowMap<String, Integer> a = new SlowMap<String, Integer>();
 		for (String string : textFile) {
 			a.put(string, a.get(string) == null ? 1 : a.get(string) + 1);
