7e5624f1a3b8014bd8b11a2e325743ee6c561865
AlexLixin <lixin1996919@126.com>
2019-08-15 23:46:38 +0800

    fast solution

--- algorithm/src/Leetcode/Leetcode703.java
+++ algorithm/src/Leetcode/Leetcode703.java
@@ -1,9 +1,6 @@
 package Leetcode;
 
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 /*
 Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
@@ -36,36 +33,26 @@ You may assume that nums' length ¡Ý k-1 and k ¡Ý 1.
  */
 class KthLargest {
 
-	private int k;
-	private int[] nums;
-	private final PriorityQueue<Integer> pq = new PriorityQueue<Integer>(new Comparator<Integer>() {
-		@Override
-		public int compare(Integer o1, Integer o2) {
-			return o1==o2?0:(o1>o2?-1:1);
-		}
-		
-	});
+	private final PriorityQueue<Integer> pq;
+	private final int k;
 
 	public KthLargest(int k, int[] nums) {
-		super();
 		this.k = k;
-		this.nums = nums;
-		for (int i : this.nums) {
-			pq.add(i);
+		this.pq = new PriorityQueue<Integer>(k);
+		for (int i : nums) {
+			add(i);
 		}
 	}
 
 	public int add(int val) {
-		pq.add(val);
-		int[] tmp = new int[k];
-		for (int i = 0; i < k; i++) {
-			tmp[i] = pq.remove();
-		}
-		for (int i : tmp) {
-			pq.add(i);
-		}
 
-		return tmp[k - 1];
+		if (pq.size() < k) {
+			pq.offer(val);
+		} else if (val > pq.peek()) {
+			pq.poll();
+			pq.offer(val);
+		}
+		return pq.peek();
 
 	}
 }
@@ -75,7 +62,7 @@ public class Leetcode703 {
 	public static void main(String[] args) {
 		int k = 3;
 		int[] arr = { 4, 5, 8, 2 };
-		KthLargest kthLargest = new KthLargest(3, arr);
+		KthLargest kthLargest = new KthLargest(k, arr);
 		System.out.println(kthLargest.add(3));// returns 4
 		System.out.println(kthLargest.add(5));// returns 5
 		System.out.println(kthLargest.add(10));// returns 5
